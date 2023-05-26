2f27e8834ab512f433d29e40a9e3207057f4882e
bea00811 <irinakozlovawork@gmail.com>
2023-05-26 14:44:12 +0300

    33

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-
-import init from './init.js';
-import './style.css';
-import './styles.scss';
-import 'bootstrap';
-
-init();
--- src/init.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-import Example from './Example.js';
-
-export default () => {
-  const element = document.getElementById('point2');
-  const obj = new Example(element);
-  obj.init();
-};
