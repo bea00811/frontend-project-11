87b216f659ead92b545397c01dbe437780993f4e
bea00811 <irinakozlovawork@gmail.com>
2023-06-04 18:29:24 +0300

    on-change

--- src/index.js
+++ src/index.js
@@ -7,33 +7,30 @@ import "bootstrap";
 import * as yup from "yup";
 import onChange from "on-change";
 
+const mystate = {
+  valuefrominput: "hello",
+  smthtype: "smth",
+  urlList: [],
+  urlList2: [],
+};
+
+const watchedState = onChange(
+  mystate,
+  function (path, value, previousValue, applyData) {
+    console.log("this:", this);
+    console.log("path:", path);
+    console.log("value:", value);
+    console.log("previousValue:", previousValue);
+    console.log("applyData:", applyData);
+  }
+);
+
 const formElement = document.getElementById("url-input");
+const form = document.getElementById("rss-form");
 
-formElement.addEventListener("input", function () {
-  const mystate = {
-    valuefrominput: this.value,
-    value: "smth",
-    urlList: [],
-  };
-
-  const watchedState = onChange(
-    mystate,
-    function (path, value, previousValue, applyData) {
-      console.log("this:", this);
-      console.log("path:", path);
-      console.log("value:", value);
-      console.log("previousValue:", previousValue);
-      console.log("applyData:", applyData);
-    }
-  );
-
-  const showValueFromState = () => {
-    console.log("watched state worked down");
-
-    console.log(watchedState.valuefrominput);
-    console.log(watchedState.value);
-    console.log(watchedState.urlList);
-  };
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  watchedState.valuefrominput = formElement.value;
 
   const putUrlsToList = () => {
     watchedState.urlList.push(watchedState.valuefrominput);
@@ -41,27 +38,24 @@ formElement.addEventListener("input", function () {
   };
 
   const schema = yup.object({
-    name: yup.string().required(),
+    name: yup.string().required().min(3, "нужно не менее 3 символов"),
   });
 
   const validDataFromInput = schema.validate(
-    { name: this.value },
+    { name: watchedState.valuefrominput },
     { strict: true }
   );
 
-  // @ts-ignore
   validDataFromInput.then(
     (result) => {
       document.getElementById("url-input").style.border = "none";
       putUrlsToList();
-      showValueFromState();
-      //   putUrlsToList();
-    }, // all is right in input!
+    },
     (error) => {
       document.getElementById("url-input").style.border = "4px solid red";
       console.log("oops!" + error);
     }
-  ); // there is errors in input!
+  );
 });
 
 init();
